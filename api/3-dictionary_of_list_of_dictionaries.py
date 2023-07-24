78e0009b9dc774595cf0003ba3a60c3df6c9a069
Josh D <joj237@gmail.com>
2023-07-23 19:17:26 -0700

    add other two files

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Python script that uses a REST API
+(https://jsonplaceholder.typicode.com/),
+to export data in the JSON format.
+"""
+import json
+import requests
+import sys
+from sys import argv
+
+if __name__ == "__main__":
+    get_id = set()
+    request = requests.get("https://jsonplaceholder.typicode.com/posts")
+    data = request.json()
+
+    for user in data:
+        get_id.add(user.get("userId"))
+    file_export = {}
+    url_u = "https://jsonplaceholder.typicode.com/users/{}"
+    url_todos = "https://jsonplaceholder.typicode.com/todos?userId={}"
+
+    for user in get_id:
+        url_users = requests.get(url_u.format(user))
+        url_name = url_users.json().get("username")
+        url_users = requests.get(url_todos.format(user))
+        data_request = url_users.json()
+        file_export['{}'.format(user)] = []
+
+        for task in data_request:
+            file_export['{}'.format(user)].append({
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": url_name
+            })
+
+        with open('todo_all_employees.json', mode='w') as file:
+            json.dump({int(x): file_export[x] for x in file_export.keys()},
+                      file, sort_keys=True)
